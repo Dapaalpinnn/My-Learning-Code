c9ae3ff28f45236b0924d962a331d4ee5b91ce6b
Dafa Alvin <162844843+Dapaalpinnn@users.noreply.github.com>
2024-03-10 07:34:50 +0700

    Menghitung Usia
    Uplload file python tentang program menghitung usia

---
+++ tahunLahir.py
@@ -0,0 +1,11 @@
+# Menghitung umur di tahun 2024 sekarang
+tahunLahir = int(input("Masukkan tahun lahirmu: "))
+usiaSekarang = 2024 - tahunLahir
+print(f"Usia kamu sekarang adalah {usiaSekarang} tahun")
+
+# Mengetahui tahun saat umur ke n
+tahunN = int(input("Masukkan usia yang kamu inginkan untuk melihat tahun berapa kamu diusia tersebut: "))
+
+# Hitung Umur
+tahunKeDepan = tahunLahir + tahunN
+print(f"Kamu akan berusia {tahunN} di tahun {tahunKeDepan}")
